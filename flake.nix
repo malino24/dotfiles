71aa721a3b03ebc008f3c42b0707423a11dffd52
Marino <marino.zirks@gmail.com>
2026-03-30 22:04:43 +0200

    no idea

--- flake.nix
+++ flake.nix
@@ -2,22 +2,50 @@
     description = "A very basic flake";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        nixpkgs = {
+            # pin-point version of repo
+            url = "github:nixos/nixpkgs/nixos-25.11";
+
+            # unstable alternative
+            # url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        };
+
+        # home-manager, used for managing user configuration
+        home-manager = {
+            url = "github:nix-community/home-manager/release-25.11";
+
+            # The `follows` keyword in inputs is used for inheritance.
+            # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+            # the `inputs.nixpkgs` of the current flake,
+            # to avoid problems caused by different versions of nixpkgs.
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = { self, nixpkgs }: {
+    outputs = { self, nixpkgs, home-manager, ... }: {
         #packages.x86_64-linux = {
           #default = self.packages.x86_64-linux.hello;
           #hello = nixpkgs.legacyPackages.x86_64-linux.hello;
         #};
 
         nixosConfigurations = {
-            MainPC = nixpkgs.lib.nixosSystem {
+            nixbtw = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
-                modules = [ ./configuration.nix ];
-                extraArgs = {
-                    inherit inputs
-                };
+
+                modules = [
+                    ./nix/configuration.nix
+
+                    home-manager.nixosModules.home-manager
+                    {
+                        home-manager.useGlobalPkgs = true;
+                        home-manager.useUserPackages = true;
+
+                        home-manager.users.user1 = import ./nix/home.nix;
+
+                        # Optionally, use home-manager.extraSpecialArgs to pass
+                        # arguments to home.nix
+                    }
+                ];
             };
         };
     };
